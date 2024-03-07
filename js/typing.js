40d3631e445a05460147d418a0c7a57cec4494ff
Benjamin Miracle Kalu <141151266+Miraculous-bmk@users.noreply.github.com>
2024-03-07 17:56:31 +0100

    upated

---
+++ js/typing.js
@@ -0,0 +1,8 @@
+
+
+var typed = new Typed(".typing",{
+    strings:["Delicious Recipes","Cooking Tips","Rich Flavors","Creamy and Utterly Decadent","Symphony of Spice"," Savory Goodness ","Golden Perfection with Fragrant Aromas"," Tropical Explosion of Sweet","Refreshing Delights and more!"],
+    typeSpeed:90,
+    BackSpeed:95,
+    loop:true
+})
